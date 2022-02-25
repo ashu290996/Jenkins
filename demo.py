3810dfcc8bf685a82064a04c86da4bb344325c95
ashu290996 <49842944+ashu290996@users.noreply.github.com>
2022-02-26 00:50:02 +0530

    Update demo.py
    4th commit

--- demo.py
+++ demo.py
@@ -1,7 +1,13 @@
 num1 ={1,5,5,10,15,20,'ashu'}
+num2 =[1,6,3,7,2] 
 for item1 in num1:
     print(item1)
 
 print(type(num1))
 num1.pop()
 print(num1)
+
+for item1 in num2:
+    print(item1)
+    
+print(type(num2))
